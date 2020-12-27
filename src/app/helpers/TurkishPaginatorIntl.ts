3a3dbc54e1d9975a0fdcb9dc3ee6d5f86ece3bd1
ouzkse <oguzkose12@gmail.com>
2020-12-27 21:33:39 +0300

    [ouzkse][Localization]

---
+++ src/app/helpers/TurkishPaginatorIntl.ts
@@ -0,0 +1,27 @@
+import { MatPaginatorIntl } from '@angular/material/paginator';
+
+const rangeLabel = (page: number, pageSize: number, length: number) => {
+  if (length === 0 || pageSize === 0) { return `0 / ${length}`; }
+
+  length = Math.max(length, 0);
+
+  const startIndex = page * pageSize;
+
+  // If the start index exceeds the list length, do not try and fix the end index to the end.
+  const endIndex = startIndex < length ?
+    Math.min(startIndex + pageSize, length) :
+    startIndex + pageSize;
+
+  return `${startIndex + 1} / ${length}`;
+};
+
+export function getTurkishPaginatorIntl() {
+  const paginatorIntl = new MatPaginatorIntl();
+
+  paginatorIntl.itemsPerPageLabel = 'Gösterilen Eleman Sayısı';
+  paginatorIntl.nextPageLabel = 'Sonraki Sayfa';
+  paginatorIntl.previousPageLabel = 'Önceki Sayfa';
+  paginatorIntl.getRangeLabel = rangeLabel;
+
+  return paginatorIntl;
+}
